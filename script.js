4b51fe8cf1cb551e8b8e41189527e46b3329803e
LauritsSchou <laurits.schou@gmail.com>
2023-04-18 15:49:42 +0200

    solved new Data Structure

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 "use strict";
-const endpoint = "https://forms-rest-crud-project-default-rtdb.europe-west1.firebasedatabase.app/";
+const endpoint =
+  "https://raw.githubusercontent.com/cederdorff/mdu-frontend/main/data/";
 window.addEventListener("load", initApp);
 async function initApp() {
   console.log("initApp is running");
   const posts = await getPosts();
   posts.forEach(showPosts);
+  const users = await getUsers();
+  users.forEach(showUsers);
 }
 async function getPosts() {
   const response = await fetch(`${endpoint}/posts.json`);
   const data = await response.json();
   const posts = preparePostData(data);
-  console.log(data);
   preparePostData(data);
   return posts;
 }
@@ -19,10 +21,8 @@ function preparePostData(dataObject) {
   for (const key in dataObject) {
     const post = dataObject[key];
     post.id = key;
-    console.log(post);
     postArray.push(post);
   }
-  console.log(postArray);
   return postArray;
 }
 function showPosts(post) {
@@ -30,23 +30,57 @@ function showPosts(post) {
   const postHTML = /*html*/ ` <article class="grid-item">
                 <img src="${post.image}">
                 <h2>${post.title}</h2>
+                <h2>${post.body}</h2>
                 
             </article>`;
   document.querySelector("#posts").insertAdjacentHTML("beforeend", postHTML);
-  document.querySelector("#posts article:last-child").addEventListener("click", clickPost);
+  document
+    .querySelector("#posts article:last-child")
+    .addEventListener("click", clickPost);
 
   function clickPost() {
     console.log("clickPost is running");
-    document.querySelector("#postdetails").showModal();
+    document.querySelector("#postDetails").showModal();
     const dialogHTML = /*html*/ `
     <h1>${post.title}</h1>
 <img src="${post.image}" class="center">
+<h2>${post.body}</h2>
     <form method="dialog">
 		<button id ="closeModalButton">Close</button>
     </form>`;
 
-    document.querySelector("#postdetails").innerHTML = dialogHTML;
+    document.querySelector("#postDetails").innerHTML = dialogHTML;
   }
+}
+async function getUsers() {
+  console.log("getUsers is running");
+  const response = await fetch(`${endpoint}/users.json`);
+  const data = await response.json();
+  const users = prepareUserData(data);
+  prepareUserData(data);
+  return users;
+}
+function prepareUserData(dataObject) {
+  console.log("prepareUserData is running");
+  const userArray = [];
+  for (const key in dataObject) {
+    const user = dataObject[key];
+    user.id = key;
+    userArray.push(user);
+  }
+  return userArray;
+}
+function showUsers(user) {
+  console.log("showUsers is running");
+  const userHTML = /*html*/ ` <article class="grid-item">
+                <img src="${user.image}">
+                <h2>${user.name}</h2>
+                <h2>${user.title}</h2>
+                <h2>${user.mail}</h2>
+                <h2>${user.phone}</h2>
+                
+            </article>`;
+  document.querySelector("#users").insertAdjacentHTML("beforeend", userHTML);
 }
 // === CREATE (POST) === //
 async function createPost(title, image) {
